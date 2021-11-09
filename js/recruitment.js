368f6bfd8ce1e016d87e7b828469ebcbcdf802e5
SAM <king.of.specktroum@gmail.com>
2021-11-09 21:25:15 +0330

    problem fixed

---
+++ js/recruitment.js
@@ -0,0 +1,22 @@
+let pageDataa = {};
+let formsss = document.querySelector('formsss').addEventListener('submit' , (e)=>{
+     
+     pageDataa ={
+          fullName: e.target.fullname.value,
+          date: e.target.date.value,
+          phonenumber: e.target.phonenumber.value,
+          gender: e.target.gender.value,
+          marriedstatus: e.target.marriedstatus.value,
+          nationalCode: e.target.nationalCode.value,
+          email: e.target.email.value,
+          fullName: e.target.fullname.value,
+          fullName: e.target.fullname.value,
+          fullName: e.target.fullname.value,
+          fullName: e.target.fullname.value,
+          fullName: e.target.fullname.value,
+          fullName: e.target.fullname.value,
+          fullName: e.target.fullname.value
+     }
+     
+     e.preventDefault()
+})
