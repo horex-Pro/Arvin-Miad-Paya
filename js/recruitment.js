fb4c09c7ac30c8b3554dd4ad689e588826c77c4f
Horex <hosseinhorex@gmail.com>
2021-11-22 16:29:37 +0330

    test

--- js/recruitment.js
+++ js/recruitment.js
@@ -1,22 +1,9 @@
-let pageDataa = {};
-let formsss = document.querySelector('formsss').addEventListener('submit' , (e)=>{
-     
-     pageDataa ={
-          fullName: e.target.fullname.value,
-          date: e.target.date.value,
-          phonenumber: e.target.phonenumber.value,
-          gender: e.target.gender.value,
-          marriedstatus: e.target.marriedstatus.value,
-          nationalCode: e.target.nationalCode.value,
-          email: e.target.email.value,
-          fullName: e.target.fullname.value,
-          fullName: e.target.fullname.value,
-          fullName: e.target.fullname.value,
-          fullName: e.target.fullname.value,
-          fullName: e.target.fullname.value,
-          fullName: e.target.fullname.value,
-          fullName: e.target.fullname.value
-     }
-     
-     e.preventDefault()
+
+
+let pageFourAddButton = document.querySelector('#pageFourAddButton');
+
+pageFourAddButton.addEventListener('click',function(){
+
+     let main = document.createElement('main');
+
 })
