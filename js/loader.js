035784e777f39579b90d66a1e57af16b722f9e52
horex-Pro <78277303+horex-Pro@users.noreply.github.com>
2021-12-15 19:55:43 +0330

    loading added

---
+++ js/loader.js
@@ -0,0 +1,10 @@
+
+
+// let loadingWrapper = document.querySelector(".loading-wrapper");
+
+
+// window.addEventListener('load' , function(){
+
+//     loadingWrapper.style.display= "none";
+
+// })
