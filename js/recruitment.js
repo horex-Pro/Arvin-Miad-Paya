af817d401d592f0829b5e6ed336eac54a25902e7
horex-Pro <78277303+horex-Pro@users.noreply.github.com>
2022-01-11 23:54:41 +0330

    some bug fixed

--- js/recruitment.js
+++ js/recruitment.js
@@ -1,98 +1,102 @@
+
+let addResumeIndicator = 0;
 function addResume(){
+    addResumeIndicator ++;   
     document.querySelector('.resume').innerHTML  +=`<div class="inputs">
-    <input type="text" class="w-50" placeholder="نام شرکت">
-    <input type="number" class="w-50" placeholder="حقوق دریافتی">
-    <input type="text" class="w-50" placeholder="زمینه فعالیت">
-    <input type="text" class="w-50" placeholder="دلیل قطع همکاری">
-    <input type="text" class="w-50" placeholder="سمت">
-    <input type="date" class="w-25" title="تاریخ شروع همکاری">
-    <input type="date" class="w-25" title="تاریخ پایان همکاری">
-    <input type="text" class="w-75" placeholder="آدرس شرکت">
+    <input type="text" class="w-50" placeholder="نام شرکت" name="jobs[${addResumeIndicator}][factory_name]">
+    <input type="number" class="w-50" placeholder="حقوق دریافتی" name="jobs[${addResumeIndicator}][rights]">
+    <input type="text" class="w-50" placeholder="زمینه فعالیت" name="jobs[${addResumeIndicator}][field]">
+    <input type="text" class="w-50" placeholder="دلیل قطع همکاری" name="jobs[${addResumeIndicator}][stop_reason]">
+    <input type="text" class="w-50" placeholder="سمت" name="jobs[${addResumeIndicator}][position]">
+    <input type="text" class="w-50" placeholder="آدرس شرکت" name="jobs[${addResumeIndicator}][address]">
+    <input type="text" class="w-100" placeholder="تاریخ شروع همکاری" name="jobs[${addResumeIndicator}][start_date]" id="startCooperation${addResumeIndicator}">
+    <input type="text" class="w-100" placeholder="تاریخ پایان همکاری" name="jobs[${addResumeIndicator}][end_date]" id="endCooperation${addResumeIndicator}">
     <input type="button" onclick="removeResume()" id="removeResumee" class="w-25" value="حذف مورد">
-</div>`;
+</div>`;    
+    addResumeIndicator++;
 }
 function removeResume() {document.querySelector('#removeResumee').parentElement.remove();}
 
-
+let addEducationIndicator = 0;
 function addEducationalBackground(){
+    addEducationIndicator++;
     document.querySelector('.educational-background').innerHTML  +=`<div class="inputs">
-    <input type="text" class="w-50" placeholder="مقطع تحصیلی">
-    <input type="number" class="w-25" placeholder="معدل">
-    <input type="text" class="w-25" placeholder="گرایش">
-    <input type="text" class="w-50" placeholder="نام دانشگاه">
-    <input type="date" class="w-25" title="تاریخ شروع">
-    <input type="date" class="w-25" title="تاریخ پایان">
-    <input type="text" class="w-50" placeholder="رشته تحصیلی">
+    <input type="text" class="w-50" placeholder="مقطع تحصیلی" name="edu[${addEducationIndicator}][degree]">
+    <input type="number" class="w-25" placeholder="معدل" name="edu[${addEducationIndicator}][GPA]" >
+    <input type="text" class="w-25" placeholder="گرایش" name="edu[${addEducationIndicator}][trend]" >
+    <input type="text" class="w-50" placeholder="نام دانشگاه" name="edu[${addEducationIndicator}][uni]">
+    <input type="text" class="w-50" placeholder="رشته تحصیلی" name="edu[${addEducationIndicator}][fields]">
+    <input type="text" class="w-75" title="تاریخ شروع" name="edu[${addEducationIndicator}][start_date]" id="startLearn${addEducationIndicator}">
+    <input type="text" class="w-75" title="تاریخ پایان" name="edu[${addEducationIndicator}][end_date]" id="endLearn${addEducationIndicator}">
     <input type="button" onclick="removeEducationalBackground()" id="removeEducationalBackgroundd" class="w-25" value="حذف مورد">
     </div>`;
+    addEducationIndicator++;
+
 }
+
 function removeEducationalBackground() {document.querySelector('#removeEducationalBackgroundd').parentElement.remove();}
 
-function addEducationalBackground(){
-    document.querySelector('.educational-background').innerHTML  +=`<div class="inputs">
-    <input type="text" class="w-50" placeholder="مقطع تحصیلی">
-    <input type="number" class="w-25" placeholder="معدل">
-    <input type="text" class="w-25" placeholder="گرایش">
-    <input type="text" class="w-50" placeholder="نام دانشگاه">
-    <input type="date" class="w-25" title="تاریخ شروع">
-    <input type="date" class="w-25" title="تاریخ پایان">
-    <input type="text" class="w-50" placeholder="رشته تحصیلی">
-    <input type="button" onclick="removeEducationalBackground()" id="removeEducationalBackgroundd" class="w-25" value="حذف مورد">
-    </div>`;
-}
-function removeEducationalBackground() {document.querySelector('#removeEducationalBackgroundd').parentElement.remove();}
-
+let addlanguageIndicator = 0;
 function addlanguage(){
+    addlanguageIndicator++;
     document.querySelector('.languages').innerHTML  +=`<div class="inputs">
-    <input type="text" class="w-100" placeholder="نام زبان">
-    <select name="" class="w-25" id="speaking">
+    <input type="text" class="w-100" placeholder="نام زبان" name="lang[${addlanguageIndicator}][name]">
+    <select name="" class="w-25" id="speaking" name="lang[${addlanguageIndicator}][conversation]">
         <option value="">مکالمه</option>
-        <option value="">عالی</option>
-        <option value="">خوب</option>
-        <option value="">متوسط</option>
-        <option value="">ضعیف</option>
+        <option value="3">عالی</option>
+        <option value="2">خوب</option>
+        <option value="1">متوسط</option>
+        <option value="0">ضعیف</option>
     </select>
-    <select name="" class="w-25" id="reading">
-        <option value="">خواندن</option>
-        <option value="">عالی</option>
-        <option value="">خوب</option>
-        <option value="">متوسط</option>
-        <option value="">ضعیف</option>
+    <select class="w-25" id="reading" name="lang[${addlanguageIndicator}][read]">
+        <option value="" disabled>خواندن</option>
+        <option value="3">عالی</option>
+        <option value="2">خوب</option>
+        <option value="1">متوسط</option>
+        <option value="0">ضعیف</option>
     </select>
-    <select name="" class="w-25" id="writing">
-        <option value="">نوشتن</option>
-        <option value="">عالی</option>
-        <option value="">خوب</option>
-        <option value="">متوسط</option>
-        <option value="">ضعیف</option>
+    <select class="w-25" id="writing" name="lang[${addlanguageIndicator}][write]">
+        <option value="" disabled>نوشتن</option>
+        <option value="3">عالی</option>
+        <option value="2">خوب</option>
+        <option value="1">متوسط</option>
+        <option value="0">ضعیف</option>
     </select>
     <input type="button" onclick="removelanguage()" id="removelanguagee" class="w-25" value="حذف مورد">
     </div>`;
+    addlanguageIndicator++;
+    
 }
 function removelanguage() {document.querySelector('#removelanguagee').parentElement.remove();}
 
+let addSoftIndicator = 0;
 function addFamiliarityWithSpecializedSoftware(){
+    addSoftIndicator++;
     document.querySelector('.familiarity-with-specialized-software').innerHTML  +=`<div class="inputs">
-    <input type="text" class="w-50" placeholder="نام نرم افزار">
-    <select name="" class="w-25" id="knowledge">
+    <input type="text" class="w-50" placeholder="نام نرم افزار" name="soft[${addSoftIndicator}][name]">
+    <select name="" class="w-25" id="knowledge" name="soft[${addSoftIndicator}][degree]">
          <option value="">میزان آشنایی</option>
-         <option value="">عالی</option>
-         <option value="">خوب</option>
-         <option value="">متوسط</option>
-         <option value="">ضعیف</option>
+         <option value="3">عالی</option>
+         <option value="2">خوب</option>
+         <option value="1">متوسط</option>
+         <option value="0">ضعیف</option>
     </select>
     <input type="button" onclick="removeFamiliarityWithSpecializedSoftware()" id="removeFamiliarityWithSpecializedSoftwaree" class="w-25" value="حذف مورد">
     </div>`;
+    addSoftIndicator++;
 }
 function removeFamiliarityWithSpecializedSoftware() {document.querySelector('#removeFamiliarityWithSpecializedSoftwaree').parentElement.remove();}
 
+let addCourseIndicator = 0;
 function addSpecializedCoursesProvided(){
+    addCourseIndicator++;
     document.querySelector('.specialized-courses-provided').innerHTML  +=`<div class="inputs">
-    <input type="text" class="w-50" placeholder="نام دوره">
-    <input type="text" class="w-50" placeholder="محل برگزاری">
-    <input type="time" class="w-25" title="مدت دوره">
-    <input type="date" class="w-25" title="تاریخ دوره">
+    <input type="text" class="w-50" placeholder="نام دوره" name="courses[${addCourseIndicator}][name]">
+    <input type="text" class="w-50" placeholder="محل برگزاری" name="courses[${addCourseIndicator}][address]">
+    <input type="time" class="w-25" title="مدت دوره" name="courses[${addCourseIndicator}][time]">
+    <input type="text" class="w-25" title="تاریخ دوره" id="coursesDate" name="courses[${addCourseIndicator}][date]">
     <input type="button" onclick="removeSpecializedCoursesProvided()" id="removeSpecializedCoursesProvidedd" class="w-25" value="حذف مورد">
     </div>`;
+    addCourseIndicator++;
 }
 function removeSpecializedCoursesProvided() {document.querySelector('#removeSpecializedCoursesProvidedd').parentElement.remove();}
